b0e85aa44d267601f56053f9ace9d14da8088278
guy.larochelle <guy@siglr.com>
2023-02-19 11:16:07 -0500

    Changing version to release 23.2.19.1

--- DiscordHelper/My Project/AssemblyInfo.vb
+++ DiscordHelper/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("23.02.17.2")>
+<Assembly: AssemblyVersion("23.02.19.1")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
