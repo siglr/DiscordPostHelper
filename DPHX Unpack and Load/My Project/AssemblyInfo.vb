7392c0b5af8ed41e2f46b475cbebdd895e8c0635
guy.larochelle <guy@siglr.com>
2023-10-21 07:26:44 -0400

    Dev test release

--- DPHX Unpack and Load/My Project/AssemblyInfo.vb
+++ DPHX Unpack and Load/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("23.10.21.1")>
-<Assembly: AssemblyFileVersion("23.10.21.1")>
+<Assembly: AssemblyVersion("23.10.21.2")>
+<Assembly: AssemblyFileVersion("23.10.21.2")>
