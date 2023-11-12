ee12ba0a4b458a38d7f0c99a26c6f800cb40c0af
guy.larochelle <guy@siglr.com>
2023-11-12 14:33:08 -0500

    Dev Test Release

--- DiscordHelper/My Project/AssemblyInfo.vb
+++ DiscordHelper/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("23.11.9.2")>
-<Assembly: AssemblyFileVersion("23.11.9.2")>
+<Assembly: AssemblyVersion("23.11.12.1")>
+<Assembly: AssemblyFileVersion("23.11.12.1")>
