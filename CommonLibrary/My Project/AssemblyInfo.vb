a9287fd198e69ce4b41c99048e762f8d1f6d2828
guy.larochelle <guy@siglr.com>
2023-12-20 17:58:43 -0500

    Yet another Dev Test Release

--- CommonLibrary/My Project/AssemblyInfo.vb
+++ CommonLibrary/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("23.12.20.2")>
-<Assembly: AssemblyFileVersion("23.12.20.2")>
+<Assembly: AssemblyVersion("23.12.20.3")>
+<Assembly: AssemblyFileVersion("23.12.20.3")>
--- DiscordHelper/My Project/AssemblyInfo.vb
+++ DiscordHelper/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("23.12.20.2")>
-<Assembly: AssemblyFileVersion("23.12.20.2")>
+<Assembly: AssemblyVersion("23.12.20.4")>
+<Assembly: AssemblyFileVersion("23.12.20.4")>
