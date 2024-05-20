9cedcaf3776e737d41ff080ed0dc0ebe420d80d0
guy.larochelle <guy@siglr.com>
2024-05-20 19:43:54 -0400

    TaskBrowserAdmin updated for SQLite database.

--- CommonLibrary/TBTaskData.vb
+++ CommonLibrary/TBTaskData.vb
@@ -1,112 +1,79 @@
-﻿Imports System.Web.ModelBinding
-Imports System.Web.Profile
-Imports System.Xml.Serialization
+﻿Public Class TBTaskData
 
-<XmlRoot("TBTaskData")>
-Public Class TBTaskData
-
-    <XmlIgnore()>
     Private Shared ReadOnly Property PrefUnits As New PreferredUnits
 
-    <XmlElement("TaskID")>
     Public Property TaskID As String
 
-    <XmlIgnore()>
     Public Property DPHXFilename As String
 
-    <XmlIgnore()>
     Public Property IsUpdate As Boolean
 
-    <XmlElement("Title")>
     Public Property Title As String
 
-    <XmlElement("LastUpdate")>
     Public Property LastUpdate As DateTime
 
-    <XmlElement("SimDateTime")>
     Public Property SimDateTime As DateTime
 
-    <XmlElement("IncludeYear")>
     Public Property IncludeYear As Boolean
 
-    <XmlElement("SimDateTimeExtraInfo")>
     Public Property SimDateTimeExtraInfo As String
 
-    <XmlElement("MainAreaPOI")>
     Public Property MainAreaPOI As String
 
-    <XmlElement("DepartureName")>
     Public Property DepartureName As String
 
-    <XmlElement("DepartureICAO")>
     Public Property DepartureICAO As String
 
-    <XmlElement("DepartureExtra")>
     Public Property DepartureExtra As String
 
-    <XmlElement("ArrivalName")>
     Public Property ArrivalName As String
 
-    <XmlElement("ArrivalICAO")>
     Public Property ArrivalICAO As String
 
-    <XmlElement("ArrivalExtra")>
     Public Property ArrivalExtra As String
 
-    <XmlElement("SoaringRidge")>
     Public Property SoaringRidge As Boolean
 
-    <XmlElement("SoaringThermals")>
     Public Property SoaringThermals As Boolean
 
-    <XmlElement("SoaringWaves")>
     Public Property SoaringWaves As Boolean
 
-    <XmlElement("SoaringDynamic")>
     Public Property SoaringDynamic As Boolean
 
-    <XmlElement("SoaringExtraInfo")>
     Public Property SoaringExtraInfo As String
 
-    <XmlElement("DurationMin")>
     Public Property DurationMin As String
 
-    <XmlElement("DurationMax")>
     Public Property DurationMax As String
 
-    <XmlIgnore()>
     Public ReadOnly Property DurationConcat As String
         Get
             Return SupportingFeatures.GetDuration(DurationMin, DurationMax)
         End Get
     End Property
 
-    <XmlElement("DurationExtraInfo")>
     Public Property DurationExtraInfo As String
 
-    <XmlElement("TaskDistance")>
     Public Property TaskDistance As String
 
-    <XmlElement("TotalDistance")>
     Public Property TotalDistance As String
 
-    <XmlIgnore()>
+    Public Property MapImage As Byte()
+
+    Public Property CoverImage As Byte()
+
     Public ReadOnly Property DistancesConcat As String
         Get
             Return SupportingFeatures.GetDistance(TotalDistance, TaskDistance, PrefUnits)
         End Get
     End Property
 
-    <XmlElement("RecommendedGliders")>
     Public Property RecommendedGliders As String
 
-    <XmlElement("DifficultyRating")>
     Public Property DifficultyRating As String
 
-    <XmlElement("DifficultyExtraInfo")>
     Public Property DifficultyExtraInfo As String
 
-    <XmlIgnore()>
     Public ReadOnly Property DifficultyConcat As String
         Get
             Dim diffIndex As Integer = CInt(DifficultyRating.Substring(0, 1))
@@ -114,28 +81,21 @@ Public Class TBTaskData
         End Get
     End Property
 
-    <XmlElement("ShortDescription")>
     Public Property ShortDescription As String
 
-    <XmlElement("LongDescription")>
     Public Property LongDescription As String
 
-    <XmlElement("WeatherSummary")>
     Public Property WeatherSummary As String
 
-    <XmlElement("Credits")>
     Public Property Credits As String
 
-    <XmlElement("Countries")>
     Public Property Countries As String
 
-    <XmlElement("RecommendedAddOns")>
     Public Property RecommendedAddOns As Boolean
 
     Public Sub New()
 
     End Sub
-
 
 End Class
 
--- TaskBrowserAdmin/AdminScreen.vb
+++ TaskBrowserAdmin/AdminScreen.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Drawing.Drawing2D
+﻿Imports System.Data.SQLite
+Imports System.Drawing.Drawing2D
 Imports System.Drawing.Imaging
 Imports System.IO
 Imports System.Xml
@@ -10,48 +11,98 @@ Public Class AdminScreen
     Private _SF As New SupportingFeatures(SupportingFeatures.ClientApp.TaskBrowserAdmin)
     Private TempDPHXUnpackFolder As String
     Private ProcessedDPHXFilesFolder As String
-    Private TBTaskDatabaseXMLFile As String
+    Private SQLiteDBFile As String
     Private _currentTaskDBEntries As New Dictionary(Of String, TBTaskData)
     Private _incomingTaskDBEntries As Dictionary(Of String, TBTaskData)
     Private _XmlDocFlightPlan As XmlDocument
 
     Private Sub AdminScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        'Set temporary unpack folder
+        ' Set temporary unpack folder
         TempDPHXUnpackFolder = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "TempUnpack")
         ProcessedDPHXFilesFolder = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "ProcessedDPHXFiles")
-        TBTaskDatabaseXMLFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "TBTaskDatabase.xml")
+        SQLiteDBFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "TasksDatabase.db")
 
-        'Read the current database file
-        SupportingFeatures.DeserializeTaskDataList(TBTaskDatabaseXMLFile, _currentTaskDBEntries)
+        ' Read the current database file
+        LoadCurrentTasksFromDatabase()
         UpdateCurrentDBGrid()
+    End Sub
 
+    Private Sub LoadCurrentTasksFromDatabase()
+        Dim connectionString As String = $"Data Source={SQLiteDBFile};Version=3;"
+        Using conn As New SQLiteConnection(connectionString)
+            conn.Open()
+            Using cmd As New SQLiteCommand("SELECT * FROM Tasks", conn)
+                Using reader As SQLiteDataReader = cmd.ExecuteReader()
+                    While reader.Read()
+                        Dim task As New TBTaskData With {
+                        .TaskID = reader("TaskID").ToString(),
+                        .Title = reader("Title").ToString(),
+                        .LastUpdate = DateTime.Parse(reader("LastUpdate").ToString()),
+                        .SimDateTime = DateTime.Parse(reader("SimDateTime").ToString()),
+                        .IncludeYear = Convert.ToBoolean(reader("IncludeYear")),
+                        .SimDateTimeExtraInfo = reader("SimDateTimeExtraInfo").ToString(),
+                        .MainAreaPOI = reader("MainAreaPOI").ToString(),
+                        .DepartureName = reader("DepartureName").ToString(),
+                        .DepartureICAO = reader("DepartureICAO").ToString(),
+                        .DepartureExtra = reader("DepartureExtra").ToString(),
+                        .ArrivalName = reader("ArrivalName").ToString(),
+                        .ArrivalICAO = reader("ArrivalICAO").ToString(),
+                        .ArrivalExtra = reader("ArrivalExtra").ToString(),
+                        .SoaringRidge = Convert.ToBoolean(reader("SoaringRidge")),
+                        .SoaringThermals = Convert.ToBoolean(reader("SoaringThermals")),
+                        .SoaringWaves = Convert.ToBoolean(reader("SoaringWaves")),
+                        .SoaringDynamic = Convert.ToBoolean(reader("SoaringDynamic")),
+                        .SoaringExtraInfo = reader("SoaringExtraInfo").ToString(),
+                        .DurationMin = Convert.ToInt32(reader("DurationMin")),
+                        .DurationMax = Convert.ToInt32(reader("DurationMax")),
+                        .DurationExtraInfo = reader("DurationExtraInfo").ToString(),
+                        .TaskDistance = Convert.ToInt32(reader("TaskDistance")),
+                        .TotalDistance = Convert.ToInt32(reader("TotalDistance")),
+                        .RecommendedGliders = reader("RecommendedGliders").ToString(),
+                        .DifficultyRating = reader("DifficultyRating").ToString(),
+                        .DifficultyExtraInfo = reader("DifficultyExtraInfo").ToString(),
+                        .ShortDescription = reader("ShortDescription").ToString(),
+                        .LongDescription = reader("LongDescription").ToString(),
+                        .WeatherSummary = reader("WeatherSummary").ToString(),
+                        .Credits = reader("Credits").ToString(),
+                        .Countries = reader("Countries").ToString(),
+                        .RecommendedAddOns = Convert.ToBoolean(reader("RecommendedAddOns"))
+                    }
+
+                        ' Load the MapImage
+                        If Not IsDBNull(reader("MapImage")) Then
+                            task.MapImage = DirectCast(reader("MapImage"), Byte())
+                        End If
+
+                        ' Load the CoverImage
+                        If Not IsDBNull(reader("CoverImage")) Then
+                            task.CoverImage = DirectCast(reader("CoverImage"), Byte())
+                        End If
+
+                        _currentTaskDBEntries.Add(task.TaskID, task)
+                    End While
+                End Using
+            End Using
+        End Using
     End Sub
 
     Private Sub UpdateCurrentDBGrid()
-
-        'Map the dictionnary as datasource to the current database grid
-        gridCurrentDatabase.DataSource = _currentTaskDBEntries.Values.ToList
+        gridCurrentDatabase.DataSource = _currentTaskDBEntries.Values.ToList()
         gridCurrentDatabase.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader
         gridCurrentDatabase.ReadOnly = True
         gridCurrentDatabase.Columns(1).Visible = False
         gridCurrentDatabase.Columns(2).Visible = False
-
     End Sub
 
     Private Sub UpdateIncomingDBGrid()
-
-        'Map the dictionnary as datasource to the incoming grid
-        gridIncomingDPHXFilesData.DataSource = _incomingTaskDBEntries.Values.ToList
+        gridIncomingDPHXFilesData.DataSource = _incomingTaskDBEntries.Values.ToList()
         gridIncomingDPHXFilesData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader
         gridIncomingDPHXFilesData.ReadOnly = True
-
     End Sub
 
     Private Sub btnSelectDPHXFiles_Click(sender As Object, e As EventArgs) Handles btnSelectDPHXFiles.Click
-
         OpenFileDialog1.InitialDirectory = "H:\MSFS WIP Flight plans\"
-
         OpenFileDialog1.FileName = ""
         OpenFileDialog1.Title = "Select DPHX files"
         OpenFileDialog1.Filter = "DPHX|*.dphx"
@@ -68,13 +119,10 @@ Public Class AdminScreen
         End If
 
         UpdateIncomingDBGrid()
-
     End Sub
 
     Private Sub LoadDPHXPackage(dphxFilename As String)
-
         Dim newDPHFile As String
-
         Dim nbrTries As Integer = 0
         Do Until nbrTries = 10
             nbrTries += 1
@@ -91,16 +139,12 @@ Public Class AdminScreen
         If File.Exists(newDPHFile) Then
             Dim serializer As New XmlSerializer(GetType(AllData))
 
-            On Error Resume Next
-
             Dim _allDPHData As AllData
-
             Using stream As New FileStream(newDPHFile, FileMode.Open)
                 _allDPHData = CType(serializer.Deserialize(stream), AllData)
             End Using
 
             If newDPHFile = String.Empty Then
-                'Invalid file loaded
                 MessageBox.Show(Me, $"The DPHX file {dphxFilename} contains an invalid DPH file.", "Loading DPHX file", MessageBoxButtons.OK, MessageBoxIcon.Error)
                 Exit Sub
             End If
@@ -110,24 +154,23 @@ Public Class AdminScreen
             End If
 
             If _allDPHData.DiscordTaskID = String.Empty Then
-                'Invalid file loaded
                 MessageBox.Show(Me, $"The DPHX file {dphxFilename} contains an invalid or missing task ID.", "Loading DPHX file", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Else
-                'Process the DPH file
-                Dim newEntry As TBTaskData = CreateDPHFileEntry(_allDPHData, dphxFilename, newDPHFile)
-                If newEntry IsNot Nothing Then
-                    _incomingTaskDBEntries.Add(_allDPHData.DiscordTaskID, newEntry)
+                If _incomingTaskDBEntries.ContainsKey(_allDPHData.DiscordTaskID) Then
+                    Console.WriteLine($"Error: Duplicate DiscordTaskID {_allDPHData.DiscordTaskID} found in file {dphxFilename}.")
                 Else
-                    MessageBox.Show(Me, $"The DPHX file {dphxFilename} already exists and is the same in the current database.", "Loading DPHX file", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Dim newEntry As TBTaskData = CreateDPHFileEntry(_allDPHData, dphxFilename, newDPHFile)
+                    If newEntry IsNot Nothing Then
+                        _incomingTaskDBEntries.Add(_allDPHData.DiscordTaskID, newEntry)
+                    Else
+                        MessageBox.Show(Me, $"The DPHX file {dphxFilename} already exists and is the same in the current database.", "Loading DPHX file", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
                 End If
             End If
-
         End If
-
     End Sub
 
     Private Function CreateDPHFileEntry(DPHData As AllData, dphxFilename As String, DPHFilename As String) As TBTaskData
-
         Dim newEntry As New TBTaskData
 
         With newEntry
@@ -167,79 +210,122 @@ Public Class AdminScreen
             End If
             .DPHXFilename = dphxFilename
 
-            'We need to open up the flight plan file to get distances
+            ' Load flight plan
             Dim totalDistance As Integer
             Dim taskDistance As Integer
             Dim possibleElevationUpdateRequired As Boolean = False
 
-            'Load flight plan
-            _XmlDocFlightPlan = New XmlDocument
-            _XmlDocFlightPlan.Load($"{Path.Combine(Path.GetDirectoryName(DPHFilename), Path.GetFileName(DPHData.FlightPlanFilename))}")
+            _XmlDocFlightPlan = New XmlDocument()
+            _XmlDocFlightPlan.Load(Path.Combine(Path.GetDirectoryName(DPHFilename), Path.GetFileName(DPHData.FlightPlanFilename)))
 
             _SF.BuildAltitudeRestrictions(_XmlDocFlightPlan, totalDistance, taskDistance, possibleElevationUpdateRequired, False)
             .TaskDistance = taskDistance
             .TotalDistance = totalDistance
+
+            ' Cover and map image
+            If DPHData.MapImageSelected <> String.Empty Then
+                .MapImage = ResizeImageAndGetBytes(Path.Combine(Path.GetDirectoryName(DPHFilename), Path.GetFileName(DPHData.MapImageSelected)), 400, 400, 25)
+            End If
+
+            If DPHData.CoverImageSelected <> String.Empty Then
+                .CoverImage = ResizeImageAndGetBytes(Path.Combine(Path.GetDirectoryName(DPHFilename), Path.GetFileName(DPHData.CoverImageSelected)), 400, 400, 25)
+            End If
         End With
 
-        'Lookup the task ID in the current database to check if it's an update or an addition
         If _currentTaskDBEntries.ContainsKey(newEntry.TaskID) Then
             newEntry.IsUpdate = True
-            'Check the update date to see if something has changed
             If _currentTaskDBEntries(newEntry.TaskID).LastUpdate = newEntry.LastUpdate Then
                 newEntry = Nothing
             End If
         End If
 
         Return newEntry
-
     End Function
 
     Private Function GetFileUpdateDateTime(filePath As String) As DateTime
-
         Dim fileInfo As New FileInfo(filePath)
-
-        ' Get the last write time
         Return fileInfo.LastWriteTime
-
     End Function
 
-    Private Sub SerializeTaskDataList()
-
-        Dim tasks As List(Of TBTaskData) = _currentTaskDBEntries.Values.ToList
-
-        ' Create an XmlSerializer for the List(Of TBTaskData)
-        Dim serializer As New XmlSerializer(GetType(List(Of TBTaskData)), New XmlRootAttribute("TBTaskDatabase"))
-
-        ' Use a StreamWriter to write the XML to file
-        Using writer As New StreamWriter(TBTaskDatabaseXMLFile)
-            serializer.Serialize(writer, tasks)
-        End Using
-
-    End Sub
-
     Private Sub btnSaveDatabase_Click(sender As Object, e As EventArgs) Handles btnSaveDatabase.Click
-
         If _incomingTaskDBEntries IsNot Nothing Then
             For Each task In _incomingTaskDBEntries.Values
                 If task.IsUpdate Then
-                    'replace the task
                     _currentTaskDBEntries(task.TaskID) = task
                 Else
                     _currentTaskDBEntries.Add(task.TaskID, task)
                 End If
-                'Copy the DPHXFile and rename it using the task ID
                 File.Copy(task.DPHXFilename, Path.Combine(ProcessedDPHXFilesFolder, $"{task.TaskID}.dphx"), True)
-
             Next
-            SerializeTaskDataList()
+
+            SaveTasksToDatabase()
             UpdateCurrentDBGrid()
             _incomingTaskDBEntries.Clear()
             UpdateIncomingDBGrid()
         End If
-
     End Sub
 
-    Public Function ResizeImageAndGetBase64(inputPath As String, maxWidth As Integer, maxHeight As Integer, quality As Long) As String
+    Private Sub SaveTasksToDatabase()
+        Dim connectionString As String = $"Data Source={SQLiteDBFile};Version=3;"
+        Using conn As New SQLiteConnection(connectionString)
+            conn.Open()
+            For Each task As TBTaskData In _currentTaskDBEntries.Values
+                Using cmd As New SQLiteCommand(conn)
+                    cmd.CommandText = "INSERT OR REPLACE INTO Tasks (TaskID, Title, LastUpdate, SimDateTime, IncludeYear, SimDateTimeExtraInfo, MainAreaPOI, DepartureName, DepartureICAO, DepartureExtra, ArrivalName, ArrivalICAO, ArrivalExtra, SoaringRidge, SoaringThermals, SoaringWaves, SoaringDynamic, SoaringExtraInfo, DurationMin, DurationMax, DurationExtraInfo, TaskDistance, TotalDistance, RecommendedGliders, DifficultyRating, DifficultyExtraInfo, ShortDescription, LongDescription, WeatherSummary, Credits, Countries, RecommendedAddOns, MapImage, CoverImage) VALUES (@TaskID, @Title, @LastUpdate, @SimDateTime, @IncludeYear, @SimDateTimeExtraInfo, @MainAreaPOI, @DepartureName, @DepartureICAO, @DepartureExtra, @ArrivalName, @ArrivalICAO, @ArrivalExtra, @SoaringRidge, @SoaringThermals, @SoaringWaves, @SoaringDynamic, @SoaringExtraInfo, @DurationMin, @DurationMax, @DurationExtraInfo, @TaskDistance, @TotalDistance, @RecommendedGliders, @DifficultyRating, @DifficultyExtraInfo, @ShortDescription, @LongDescription, @WeatherSummary, @Credits, @Countries, @RecommendedAddOns, @MapImage, @CoverImage)"
+
+                    cmd.Parameters.AddWithValue("@TaskID", task.TaskID)
+                    cmd.Parameters.AddWithValue("@Title", task.Title)
+                    cmd.Parameters.AddWithValue("@LastUpdate", task.LastUpdate)
+                    cmd.Parameters.AddWithValue("@SimDateTime", task.SimDateTime)
+                    cmd.Parameters.AddWithValue("@IncludeYear", task.IncludeYear)
+                    cmd.Parameters.AddWithValue("@SimDateTimeExtraInfo", task.SimDateTimeExtraInfo)
+                    cmd.Parameters.AddWithValue("@MainAreaPOI", task.MainAreaPOI)
+                    cmd.Parameters.AddWithValue("@DepartureName", task.DepartureName)
+                    cmd.Parameters.AddWithValue("@DepartureICAO", task.DepartureICAO)
+                    cmd.Parameters.AddWithValue("@DepartureExtra", task.DepartureExtra)
+                    cmd.Parameters.AddWithValue("@ArrivalName", task.ArrivalName)
+                    cmd.Parameters.AddWithValue("@ArrivalICAO", task.ArrivalICAO)
+                    cmd.Parameters.AddWithValue("@ArrivalExtra", task.ArrivalExtra)
+                    cmd.Parameters.AddWithValue("@SoaringRidge", task.SoaringRidge)
+                    cmd.Parameters.AddWithValue("@SoaringThermals", task.SoaringThermals)
+                    cmd.Parameters.AddWithValue("@SoaringWaves", task.SoaringWaves)
+                    cmd.Parameters.AddWithValue("@SoaringDynamic", task.SoaringDynamic)
+                    cmd.Parameters.AddWithValue("@SoaringExtraInfo", task.SoaringExtraInfo)
+                    cmd.Parameters.AddWithValue("@DurationMin", task.DurationMin)
+                    cmd.Parameters.AddWithValue("@DurationMax", task.DurationMax)
+                    cmd.Parameters.AddWithValue("@DurationExtraInfo", task.DurationExtraInfo)
+                    cmd.Parameters.AddWithValue("@TaskDistance", task.TaskDistance)
+                    cmd.Parameters.AddWithValue("@TotalDistance", task.TotalDistance)
+                    cmd.Parameters.AddWithValue("@RecommendedGliders", task.RecommendedGliders)
+                    cmd.Parameters.AddWithValue("@DifficultyRating", task.DifficultyRating)
+                    cmd.Parameters.AddWithValue("@DifficultyExtraInfo", task.DifficultyExtraInfo)
+                    cmd.Parameters.AddWithValue("@ShortDescription", task.ShortDescription)
+                    cmd.Parameters.AddWithValue("@LongDescription", task.LongDescription)
+                    cmd.Parameters.AddWithValue("@WeatherSummary", task.WeatherSummary)
+                    cmd.Parameters.AddWithValue("@Credits", task.Credits)
+                    cmd.Parameters.AddWithValue("@Countries", task.Countries)
+                    cmd.Parameters.AddWithValue("@RecommendedAddOns", task.RecommendedAddOns)
+
+                    ' Handle BLOB fields
+                    If task.MapImage IsNot Nothing Then
+                        cmd.Parameters.Add("@MapImage", DbType.Binary).Value = task.MapImage
+                    Else
+                        cmd.Parameters.Add("@MapImage", DbType.Binary).Value = DBNull.Value
+                    End If
+
+                    If task.CoverImage IsNot Nothing Then
+                        cmd.Parameters.Add("@CoverImage", DbType.Binary).Value = task.CoverImage
+                    Else
+                        cmd.Parameters.Add("@CoverImage", DbType.Binary).Value = DBNull.Value
+                    End If
+
+                    cmd.ExecuteNonQuery()
+                End Using
+            Next
+        End Using
+    End Sub
+
+    Private Function ResizeImageAndGetBytes(inputPath As String, maxWidth As Integer, maxHeight As Integer, quality As Long) As Byte()
         ' Load the original image
         Using image As Image = Image.FromFile(inputPath)
             ' Calculate the new size maintaining aspect ratio
@@ -274,10 +360,7 @@ Public Class AdminScreen
                     thumbnail.Save(ms, jpgEncoder, myEncoderParameters)
 
                     ' Convert the image to a byte array
-                    Dim imageBytes As Byte() = ms.ToArray()
-
-                    ' Convert byte array to Base64 string
-                    Return Convert.ToBase64String(imageBytes)
+                    Return ms.ToArray()
                 End Using
             End Using
         End Using
