d04be843306eb6628e13f1c3807d6691f1bbf718
guy.larochelle <guy@siglr.com>
2025-01-31 11:24:19 -0500

    DevTestRelease 25.1.31.1

--- CommonLibrary/My Project/AssemblyInfo.vb
+++ CommonLibrary/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("25.1.28.3")>
-<Assembly: AssemblyFileVersion("25.1.28.3")>
+<Assembly: AssemblyVersion("25.1.31.1")>
+<Assembly: AssemblyFileVersion("25.1.31.1")>
