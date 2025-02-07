91734a2466275167b6774e590f097fb73e7ded7e
guy.larochelle <guy@siglr.com>
2025-02-07 14:25:50 -0500

    DevTestRelease 25.2.7.1 for DPHX

--- CommonLibrary/My Project/AssemblyInfo.vb
+++ CommonLibrary/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("25.2.3.3")>
-<Assembly: AssemblyFileVersion("25.2.3.3")>
+<Assembly: AssemblyVersion("25.2.7.1")>
+<Assembly: AssemblyFileVersion("25.2.7.1")>
