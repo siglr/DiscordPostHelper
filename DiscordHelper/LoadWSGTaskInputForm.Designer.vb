50ad0aed943362a8175a435f420ef8911568dc32
guy.larochelle <guy@siglr.com>
2025-02-19 08:54:06 -0500

    Possible fix for the scaling of WSG load task popup screen.

--- DiscordHelper/LoadWSGTaskInputForm.Designer.vb
+++ DiscordHelper/LoadWSGTaskInputForm.Designer.vb
@@ -26,16 +26,20 @@ Partial Class LoadWSGTaskInputForm
         Me.btnOk = New System.Windows.Forms.Button()
         Me.lblPrompt = New System.Windows.Forms.Label()
         Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnCancel
         '
         Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnCancel.Location = New System.Drawing.Point(284, 56)
+        Me.btnCancel.Location = New System.Drawing.Point(250, 3)
         Me.btnCancel.Name = "btnCancel"
         Me.btnCancel.Size = New System.Drawing.Size(104, 34)
-        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.TabIndex = 1
         Me.btnCancel.Text = "Cancel"
         Me.btnCancel.UseVisualStyleBackColor = True
         '
@@ -43,50 +47,81 @@ Partial Class LoadWSGTaskInputForm
         '
         Me.btnOk.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.btnOk.Location = New System.Drawing.Point(173, 56)
+        Me.btnOk.Location = New System.Drawing.Point(140, 3)
         Me.btnOk.Name = "btnOk"
         Me.btnOk.Size = New System.Drawing.Size(104, 34)
-        Me.btnOk.TabIndex = 2
+        Me.btnOk.TabIndex = 0
         Me.btnOk.Text = "Ok"
         Me.btnOk.UseVisualStyleBackColor = True
         '
         'lblPrompt
         '
         Me.lblPrompt.AutoSize = True
-        Me.lblPrompt.Location = New System.Drawing.Point(12, 12)
+        Me.lblPrompt.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lblPrompt.Location = New System.Drawing.Point(3, 0)
         Me.lblPrompt.Name = "lblPrompt"
-        Me.lblPrompt.Size = New System.Drawing.Size(270, 20)
+        Me.lblPrompt.Size = New System.Drawing.Size(270, 33)
         Me.lblPrompt.TabIndex = 0
         Me.lblPrompt.Text = "Enter the WSG Task ID you want to load:"
-        Me.lblPrompt.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        Me.lblPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'txtInput
         '
-        Me.txtInput.Location = New System.Drawing.Point(316, 9)
+        Me.txtInput.Location = New System.Drawing.Point(279, 3)
         Me.txtInput.Name = "txtInput"
         Me.txtInput.Size = New System.Drawing.Size(72, 27)
         Me.txtInput.TabIndex = 1
         Me.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoSize = True
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnCancel)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnOk)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 67)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(357, 40)
+        Me.FlowLayoutPanel1.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.txtInput, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPrompt, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(357, 33)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
         'LoadWSGTaskInputForm
         '
         Me.AcceptButton = Me.btnOk
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(110.0!, 110.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
         Me.CancelButton = Me.btnCancel
-        Me.ClientSize = New System.Drawing.Size(400, 102)
+        Me.ClientSize = New System.Drawing.Size(357, 107)
         Me.ControlBox = False
-        Me.Controls.Add(Me.lblPrompt)
-        Me.Controls.Add(Me.txtInput)
-        Me.Controls.Add(Me.btnCancel)
-        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
         Me.Font = New System.Drawing.Font("Segoe UI Variable Display", 9.818182!)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MinimumSize = New System.Drawing.Size(375, 125)
         Me.Name = "LoadWSGTaskInputForm"
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Loading task from WSG"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -96,4 +131,6 @@ Partial Class LoadWSGTaskInputForm
     Friend WithEvents btnOk As Button
     Friend WithEvents lblPrompt As Label
     Friend WithEvents txtInput As TextBox
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
