b90d90fc5ee94cc71652cb8073fc875891559769
guy.larochelle <guy@siglr.com>
2025-02-27 14:22:07 -0500

    Removing config php file

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'databasePath' => '/home2/siglr3/private/DPHXTaskBrowser/TasksDatabase.db',
-    'newsDBPath' => '/home2/siglr3/private/DPHXTaskBrowser/News.db',
-    'logFile' => '/home2/siglr3/private/DPHXTaskBrowser/error_log.txt',
-    'userPermissionsPath' => '/home2/siglr3/private/DPHXTaskBrowser/UserRights.xml',
-    'soaringClubsPath' => '/home2/siglr3/private/DPHXTaskBrowser/SoaringClubs.xml',
-    'fileRootPath' => '/home2/siglr3/public_html/DiscordPostHelper/',
-];
