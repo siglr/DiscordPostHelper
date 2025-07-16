0a0088995b94a96a095b4ba261d5fb38be45fbb9
guy.larochelle <guy@siglr.com>
2025-07-16 16:01:12 -0400

    Prevented errors

--- DPHX Unpack and Load/ApplicationEvents.vb
+++ DPHX Unpack and Load/ApplicationEvents.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Windows.Forms
 Imports CefSharp
 Imports CefSharp.WinForms
 
@@ -8,31 +9,38 @@ Namespace My
         Private Sub MyApplication_Startup(
             sender As Object,
             e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs
-          ) Handles Me.Startup
+        ) Handles Me.Startup
+            Try
+                ' Base folder where your EXE and all the CefSharp bits live
+                Dim baseDir = AppDomain.CurrentDomain.SetupInformation.ApplicationBase
 
-            ' Base folder where your EXE and all the CefSharp bits live
-            Dim baseDir = AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+                ' Configure CefSettings
+                Dim settings = New CefSettings With {
+                    .CachePath = Path.Combine(baseDir, "cef_cache_DPHX"),
+                    .BrowserSubprocessPath = Path.Combine(baseDir, "CefSharp.BrowserSubprocess.exe"),
+                    .ResourcesDirPath = baseDir,
+                    .LocalesDirPath = Path.Combine(baseDir, "locales"),
+                    .Locale = "en-US",
+                    .LogSeverity = LogSeverity.Error
+                }
 
-            ' Configure CefSettings
-            Dim settings = New CefSettings With {
-                                          .CachePath = Path.Combine(baseDir, "cef_cache_DPHX"),
-                                          .BrowserSubprocessPath = Path.Combine(baseDir, "CefSharp.BrowserSubprocess.exe"),
-                                          .ResourcesDirPath = baseDir,
-                                          .LocalesDirPath = Path.Combine(baseDir, "locales"),
-                                          .Locale = "en-US",
-                                          .LogSeverity = LogSeverity.Error
-}
+                settings.CefCommandLineArgs.Add("disable-extensions", "1")
+                settings.CefCommandLineArgs.Add("disable-blink-features", "WebUSB")
+                settings.CefCommandLineArgs.Add("disable-gcm", "1")
+                settings.CefCommandLineArgs.Add("disable-push-api", "1")
 
-            ' you already have this:
-            settings.CefCommandLineArgs.Add("disable-extensions", "1")
+                Cef.Initialize(settings, performDependencyCheck:=True, browserProcessHandler:=Nothing)
 
-            ' add these three:
-            settings.CefCommandLineArgs.Add("disable-blink-features", "WebUSB")
-            settings.CefCommandLineArgs.Add("disable-gcm", "1")
-            settings.CefCommandLineArgs.Add("disable-push-api", "1")
-
-            Cef.Initialize(settings, performDependencyCheck:=True, browserProcessHandler:=Nothing)
-
+            Catch ex As Exception
+                ' Show the error and stop the app from continuing
+                MessageBox.Show(
+                    $"Failed to start application:{Environment.NewLine}{ex.Message}",
+                    "Startup Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                )
+                e.Cancel = True
+            End Try
         End Sub
 
     End Class
--- DPHX Unpack and Load/My Project/AssemblyInfo.vb
+++ DPHX Unpack and Load/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("25.7.16.6")>
-<Assembly: AssemblyFileVersion("25.7.16.6")>
+<Assembly: AssemblyVersion("25.7.16.8")>
+<Assembly: AssemblyFileVersion("25.7.16.8")>
