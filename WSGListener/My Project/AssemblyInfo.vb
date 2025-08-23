a1569a1a9f08f80767c33540194f76592d2fbff0
guy.larochelle <guy@siglr.com>
2025-08-22 21:59:52 -0400

    DevTestRelease 25.8.22.2

--- WSGListener/My Project/AssemblyInfo.vb
+++ WSGListener/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("25.8.22.1")>
-<Assembly: AssemblyFileVersion("25.8.22.1")>
+<Assembly: AssemblyVersion("25.8.22.2")>
+<Assembly: AssemblyFileVersion("25.8.22.2")>
