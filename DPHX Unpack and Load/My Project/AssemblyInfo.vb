b01a480351c3353c4212d1d4e66a6ba64617c37f
guy.larochelle <guy@siglr.com>
2025-08-24 10:53:21 -0400

    DevTestRelease 25.8.24.1

--- DPHX Unpack and Load/My Project/AssemblyInfo.vb
+++ DPHX Unpack and Load/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("25.8.22.2")>
-<Assembly: AssemblyFileVersion("25.8.22.2")>
+<Assembly: AssemblyVersion("25.8.24.1")>
+<Assembly: AssemblyFileVersion("25.8.24.1")>
--- WSGListener/My Project/AssemblyInfo.vb
+++ WSGListener/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("25.8.22.2")>
-<Assembly: AssemblyFileVersion("25.8.22.2")>
+<Assembly: AssemblyVersion("25.8.24.1")>
+<Assembly: AssemblyFileVersion("25.8.24.1")>
