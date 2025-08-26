3ec2f263388b80b5738f059e3aa5a33a51b6b59b
guy.larochelle <guy@siglr.com>
2025-08-25 21:33:29 -0400

    DevTestRelease for DPHX 25.8.25.1

--- WSGListener/My Project/AssemblyInfo.vb
+++ WSGListener/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("25.8.24.1")>
-<Assembly: AssemblyFileVersion("25.8.24.1")>
+<Assembly: AssemblyVersion("25.8.25.1")>
+<Assembly: AssemblyFileVersion("25.8.25.1")>
