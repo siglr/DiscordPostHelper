3d58524c3c48729b7534cc21e006efe8d5e32eab
Guy LaRochelle <49131205+siglr@users.noreply.github.com>
2025-10-31 21:18:30 -0400

    Split NB21 logger into library and host app

---
+++ NB21_logger/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NB21 Logger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NB21 Logger")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59DA62E1-8D02-4C62-8743-A9C82B3F53D2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
