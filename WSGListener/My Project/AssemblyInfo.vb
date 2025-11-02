5f436e338ad3e67deb99bfe9d20942338646021c
guy.larochelle <guy@siglr.com>
2025-11-02 10:52:52 -0500

    Moving to 64 bits

--- WSGListener/My Project/AssemblyInfo.vb
+++ WSGListener/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("25.10.28.1")>
-<Assembly: AssemblyFileVersion("25.10.28.1")>
+<Assembly: AssemblyVersion("25.11.2.2")>
+<Assembly: AssemblyFileVersion("25.11.2.2")>
