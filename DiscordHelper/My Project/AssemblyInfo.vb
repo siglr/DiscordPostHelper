bf3dbc6c4f156e9e16347c45af4880ad06f02256
guy.larochelle <guy@siglr.com>
2025-11-13 19:12:00 -0500

    DevTestRelease 25.11.13.2 DPHX

--- DiscordHelper/My Project/AssemblyInfo.vb
+++ DiscordHelper/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("25.11.13.1")>
-<Assembly: AssemblyFileVersion("25.11.13.1")>
+<Assembly: AssemblyVersion("25.11.13.2")>
+<Assembly: AssemblyFileVersion("25.11.13.2")>
