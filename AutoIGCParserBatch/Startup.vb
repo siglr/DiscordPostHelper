f7b0748a9bd30ea290a8ab70435efebe7a993e87
Guy LaRochelle <49131205+siglr@users.noreply.github.com>
2026-01-14 16:27:25 -0500

    Log unhandled exceptions in AutoIGCParserBatch startup

--- AutoIGCParserBatch/Startup.vb
+++ AutoIGCParserBatch/Startup.vb
@@ -5,6 +5,10 @@ Imports System.Windows.Forms
 Module Startup
     <STAThread()>
     Public Sub Main()
+        AddHandler Application.ThreadException, AddressOf OnUiThreadException
+        AddHandler AppDomain.CurrentDomain.UnhandledException, AddressOf OnUnhandledException
+        AddHandler Threading.Tasks.TaskScheduler.UnobservedTaskException, AddressOf OnUnobservedTaskException
+
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
 
@@ -24,5 +28,31 @@ Module Startup
 
 
         Application.Run(New frmDiscord())
+    End Sub
+
+    Private Sub OnUiThreadException(sender As Object, e As Threading.ThreadExceptionEventArgs)
+        LogUnhandledException("UI thread exception", e.Exception)
+    End Sub
+
+    Private Sub OnUnhandledException(sender As Object, e As UnhandledExceptionEventArgs)
+        Dim ex = TryCast(e.ExceptionObject, Exception)
+        LogUnhandledException("Unhandled exception", ex)
+    End Sub
+
+    Private Sub OnUnobservedTaskException(sender As Object, e As Threading.Tasks.UnobservedTaskExceptionEventArgs)
+        LogUnhandledException("Unobserved task exception", e.Exception)
+        e.SetObserved()
+    End Sub
+
+    Private Sub LogUnhandledException(context As String, ex As Exception)
+        Try
+            Dim baseDir = AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+            Dim logPath = Path.Combine(baseDir, "AutoIGCParserBatch.unhandled.log")
+            Dim message = $"[{DateTime.UtcNow:u}] {context}{Environment.NewLine}{ex}{Environment.NewLine}"
+            File.AppendAllText(logPath, message)
+            MessageBox.Show($"{context}.{Environment.NewLine}Details logged to:{Environment.NewLine}{logPath}", "AutoIGCParserBatch Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch
+            ' Swallow any logging failures to avoid recursive crashes.
+        End Try
     End Sub
 End Module
