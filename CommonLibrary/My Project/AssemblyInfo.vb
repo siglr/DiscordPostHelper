b16c816114432c33f0a166dc9a95ec94e856857d
guy.larochelle <guy@siglr.com>
2026-01-23 08:43:36 -0500

    DevTestRelease 26.1.23.1

--- CommonLibrary/My Project/AssemblyInfo.vb
+++ CommonLibrary/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("26.1.22.2")>
-<Assembly: AssemblyFileVersion("26.1.22.2")>
+<Assembly: AssemblyVersion("26.1.23.1")>
+<Assembly: AssemblyFileVersion("26.1.23.1")>
--- WSGListener/My Project/AssemblyInfo.vb
+++ WSGListener/My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("26.1.22.2")>
-<Assembly: AssemblyFileVersion("26.1.22.2")>
+<Assembly: AssemblyVersion("26.1.23.1")>
+<Assembly: AssemblyFileVersion("26.1.23.1")>
